0601c52fa64181d6e54ae3254fef57452e4659a5
nikicuocci <nikicuocci@gmail.com>
2026-04-14 19:33:13 +0200

    S2L2

---
+++ U1S2L2/EX S2L2 Python.py
@@ -0,0 +1,19 @@
+# 1 Ho inserito il comando print per visualizzare il messagio sullo schermo#
+# EXTRA Ho inserito "-" tramite il comando print moltiplicando per 40 per ottenere un miglior desing#
+print( "-" * 40)# Parte superiore #
+print("Ciao! Benvenuto nel generatore di nomi per band\n") # il comando \n serve per andare a capo #
+print( "-" * 40)# Parte Inferiore #
+
+# 2 Ho inserito le variabili inserendo il comando imput che mi permettera di ricevere una risposta dal utente #
+animale = input("Come si chiama il tuo animale domestico ?\n")
+citta = input("Dove abiti ?\n")
+
+# 3 faccio la somma delle variabili aggiungendo uno spazio #
+band = animale + " " + citta
+
+# 4  Ho inserito un comando print per dare la stampare la risposta all utente #
+print("°" * 40) # EXTRA #
+
+print("Il nome della tua band e : \n" + band)
+
+print("°" * 40) # EXTRA #
